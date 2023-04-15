9b6fb43c2367d14cf4601184f6bdcd335a60d520
Dilano Bo <dilano.bo@protonmail.com>
2023-04-16 05:17:57 +0530

    update config

--- splashx-backend/hardhat.config.ts
+++ splashx-backend/hardhat.config.ts
@@ -4,6 +4,41 @@ import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    scroll: {
+      url: 'https://alpha-rpc.scroll.io/l2',
+      chainId: 534353,
+      accounts: [process.env.PRIVATE_KEY!],
+      gasPrice: 1e11
+    },
+    chiado: {
+      url: 'https://rpc.chiadochain.net/',
+      chainId: 10200,
+      accounts: [process.env.PRIVATE_KEY!],
+      gasPrice: 1e11
+    },
+    taiko: {
+      url: 'https://l2rpc.hackathon.taiko.xyz',
+      chainId: 167002,
+      accounts: [process.env.PRIVATE_KEY!],
+      gasPrice: 1e11
+    },
+    celo: {
+      url: 'https://forno.celo.org',
+      chainId: 42220,
+      accounts: [process.env.PRIVATE_KEY!],
+      gasPrice: 1e11
+    }
+    // mantle: {},
+  }
 };
 
 export default config;
