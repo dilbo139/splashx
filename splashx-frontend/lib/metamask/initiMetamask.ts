9c7324141c2ea037bae4de954d298469b68519ef
Dilano Bo <dilano.bo@protonmail.com>
2023-04-15 14:09:40 +0530

    tweak: add prettier config

---
+++ splashx-frontend/lib/metamask/initiMetamask.ts
@@ -0,0 +1,9 @@
+import MetaMaskSDK from "@metamask/sdk";
+
+const options = {};
+
+const MMSDK = new MetaMaskSDK(options);
+
+const ethereum = MMSDK.getProvider(); // You can also access via window.ethereum
+
+ethereum.request({ method: "eth_requestAccounts", params: [] });
